13dd6fc24d136762086080006c367e4c0e290348
hrishi2003 <hrishifreelance03@gmail.com>
2022-04-15 21:06:45 +0530

    si usd

--- aj/custom/si.py
+++ aj/custom/si.py
@@ -33,15 +33,21 @@ def ac(self,method=None):
         else:
             gl = frappe.get_doc('GL Entry',{'account':self.debit_to,'voucher_no':self.name,'posting_date':self.posting_date})
             gl.debit = gl.debit - self.base_total_taxes_and_charges
-            print('gllllllllllllllllllllll',gl.voucher_no,'gggggggggggggggggggggggggggggggggcvvvvvvvvvv%%%%%%%%%%')
-            print('gllllllllllllllllllllll',gl.debit,'gggggggggggggggggggggggggggggggggcvvvvvvvvvv%%%%%%%%%%')
+            gl.debit_in_account_currency = gl.debit_in_account_currency - self.total_taxes_and_charges
+            # gl.credit_in_account_currency = gl.credit_in_account_currency - self.total_taxes_and_charges
+            print('gllllllllllllllllllllll',gl.debit_in_account_currency,'gggggggggggggggggggggggggggggggggcvvvvvvvvvv%%%%%%%%%%')
+            # print('gllllllllllllllllllllll',gl.credit_in_account_currency,'gggggggggggggggggggggggggggggggggcvvvvvvvvvv%%%%%%%%%%')
             frappe.db.set_value('GL Entry', {'account':self.debit_to,'voucher_no':self.name,'posting_date':self.posting_date}, 'debit', gl.debit)
+            frappe.db.set_value('GL Entry', {'account':self.debit_to,'voucher_no':self.name,'posting_date':self.posting_date},'debit_in_account_currency',gl.debit_in_account_currency)
+            # frappe.db.set_value('GL Entry', {'account':self.debit_to,'voucher_no':self.name,'posting_date':self.posting_date},'credit_in_account_currency',gl.credit_in_account_currency)
             default_company = frappe.db.get_single_value('Global Defaults', 'default_company')
             doc = frappe.new_doc('GL Entry')
             doc.account = self.gst_account
             doc.voucher_no = self.name
             doc.voucher_type = 'Sales Invoice'
             doc.debit = self.base_total_taxes_and_charges
+            doc.debit_in_account_currency = self.total_taxes_and_charges
+            # doc.credit_in_account_currency = self.total_taxes_and_charges
             # doc.against = self.customer
             doc.party_type = self.party_type
             doc.posting_date = self.posting_date
